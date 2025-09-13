17630204fbccace8d39aba2ed3a964452e30dc06
devarogundade <devarogundade@gmail.com>
2025-09-13 10:00:43 +0100

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,38 @@
+import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(({ mode }) => ({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: [
+        "icons/favicon.ico",
+        "icons/apple-touch-icon.png",
+        "icons/mask-icon.svg",
+      ],
+      manifest: {
+        name: "Nomee",
+        short_name: "Nomee",
+        description: "Web3 SocialFi dApp",
+        theme_color: "#000000",
+        icons: [
+          {
+            src: "icons/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icons/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
